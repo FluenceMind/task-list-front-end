2d658248ace0a79ac829f1bfa94c35926c42004e
DayDayUp <bipentium25@gmail.com>
2025-12-18 08:39:37 -0800

    wave -04

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import TaskList from './components/TaskList.jsx';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
+import axios from 'axios';
 
 const TASKS = [
   {
@@ -17,10 +18,29 @@ const TASKS = [
 
 const App = () => {
   const [tasks, setTasks] = useState(TASKS);
+  const [errorMessage, setErrorMessage] = useState('');
+  //wave 04
+  useEffect(() => {
+    axios.get('http://localhost:5000/tasks')
+      .then((response) => {
+        setTasks(response.data);
+      })
+      .catch((error) => {
+        setErrorMessage(<section>{error.response.data.message}</section>);
+      });
+  }, []);
+
+
+  // wave 03
   const toggleTaskComplete = (taskId) =>{
     setTasks(prevTasks =>{
       return prevTasks.map((task) => {
         if (task.id === taskId) {
+          if (task.isComplete){
+            axios.patch(`http://localhost:5000/tasks/${task.id}/mark_incomplete`);
+          }else{
+            axios.patch(`http://localhost:5000/tasks/${task.id}/mark_complete`);
+          }
           return { ...task, isComplete: !task.isComplete };
         }
         return task;
@@ -28,7 +48,10 @@ const App = () => {
     });
   };
 
+
   const deleteTask = (taskId) => {
+    axios.delete(`http://localhost:5000/tasks/${taskId}`);
+
     setTasks((prevTasks) => {
       return prevTasks.filter((task) => task.id !== taskId);
     });
